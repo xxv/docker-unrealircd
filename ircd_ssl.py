cf89895cabb4d3ce2382af05ebe44c29c9351e25
Steve Pomeroy <steve@staticfree.info>
2017-01-26 23:06:01 -0500

    Change domain to Foonetic

--- ircd_ssl.py
+++ ircd_ssl.py
@@ -14,7 +14,7 @@ sock.listen(10)
 while 1:
     conn, addr = sock.accept()
     print 'Connected with ' + addr[0] + ':' + str(addr[1])
-    conn.send(':irc.hellface.com NOTICE * :*** Please reconnect on port 6697 with SSL enabled.\n');
+    conn.send(':irc.foonetic.net NOTICE * :*** Please reconnect on port 6697 with SSL enabled.\n');
     conn.close()
     
 sock.close()
